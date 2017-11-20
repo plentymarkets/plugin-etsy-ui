14455b937e1af756c669484c5e7c60ab40910907
Tudor-Dan Ravoiu <tudor.ravoiu@plentymarkets.com>
2017-11-20 10:30:35 +0100

    - fixed localisation config

--- src/app/core/localization/localization.config.ts
+++ src/app/core/localization/localization.config.ts
@@ -20,7 +20,7 @@ export class LocalizationConfig
             this.translation
                 .addConfiguration()
                 .addProvider('assets/lang/locale_')
-                .addProvider('node_modules/@plentymarkets/terra-components/app/assets/lang/locale-');
+                .addProvider('assets/lang/terra-components/locale-');
         }
         else
         {
